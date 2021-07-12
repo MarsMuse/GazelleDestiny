e67b79d83fefbc02019d790a497a724050d3bebe
zouyao <zouyao_cd@keruyun.com>
2021-07-12 20:26:32 +0800

    feat(global): 动态脚本

--- aviator/src/main/java/com/artisan/aviator/AviatorApp.java
+++ aviator/src/main/java/com/artisan/aviator/AviatorApp.java
@@ -1,5 +1,8 @@
 package com.artisan.aviator;
 
+import com.googlecode.aviator.AviatorEvaluator;
+import com.googlecode.aviator.Expression;
+
 /**
  * AviatorScript 实践
  *
@@ -9,6 +12,7 @@ package com.artisan.aviator;
 public class AviatorApp {
 
     public static void main(String[] args) {
-
+        Expression script = AviatorEvaluator.getInstance().compile("println('Hello, AviatorScript!');");
+        script.execute();
     }
 }
